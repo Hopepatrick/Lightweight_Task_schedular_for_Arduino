8a4c856555776d5b6b14dd921f788126e8f14667
Patrick Hope <113066124+Hopepatrick@users.noreply.github.com>
2026-09-20 14:22:49 +0200

    Add files via upload

---
+++ MAKE_YOUR_OWN_LIGHTWEIGHT_NON-BLOCKING_RTOS_FOR_ARDUINO.ino
@@ -0,0 +1,45 @@
+void task1(void){
+  Serial.println("I'm running as task 1");
+}
+
+void task2(void){
+  Serial.println("I'm running as task 2");
+}
+
+void task3(void){
+  Serial.println("I'm running as task 3");
+}
+
+typedef struct Scheduler{
+void (*run) (void);
+unsigned long current_time;
+unsigned long prev_time;
+uint32_t delay_time;
+};
+
+static Scheduler sched[] = {
+{task1,0,0,500},
+{task2,0,0,1000},
+{task3,0,0,2000},
+};
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i = 0; i < sizeof(sched)/ sizeof(sched[0]); i++){
+    sched[i].current_time = millis();
+  }
+  for(int i=0;i<sizeof(sched)/ sizeof(sched[0]);i++){
+    if(sched[i].current_time - sched[i].prev_time>=sched[i].delay_time){
+      sched[i].prev_time = sched[i].current_time;
+      sched[i].run();
+    }
+
+  }
+
+}
